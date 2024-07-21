d97ab112b40eb00ad0cceec831437a1b06fa18ed
DmitriyArcher1 <d.kartrajt@gmail.com>
2024-07-21 20:35:03 +0300

    Реализована рабочая пагинация по страницам.

--- goods/urls.py
+++ goods/urls.py
@@ -6,6 +6,8 @@ from goods import views
 app_name = 'goods'
 
 urlpatterns = [
-    path('', views.catalog, name = 'index'),
+    path('<slug:category_slug>/', views.catalog, name = 'index'),
+    path('<slug:category_slug>/<int:page>/', views.catalog, name = 'index'), 
+    # url-маршрутизация для отображения страницы по целому значению 
     path('product/<slug:product_slug>/', views.product, name = 'product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
 from goods.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug, page=1):
 
-    goods = Products.objects.all() # получаем все товары из БД
+    if category_slug == "all":
+        goods = Products.objects.all() # получение всех товаров из базы данных
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug)) 
+        # иначе получаем товары по определенной категории
+    
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context = {
         "title": "HC - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
