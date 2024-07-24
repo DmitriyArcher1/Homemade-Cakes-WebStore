610467c5c3fe22ae7d9e7a5ba84aa6dcf933c778
DmitriyArcher1 <d.kartrajt@gmail.com>
2024-07-24 13:56:32 +0300

    Вторая версия пагинации. Добавлены GET параметры.

--- goods/urls.py
+++ goods/urls.py
@@ -7,7 +7,6 @@ app_name = 'goods'
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name = 'index'),
-    path('<slug:category_slug>/<int:page>/', views.catalog, name = 'index'), 
-    # url-маршрутизация для отображения страницы по целому значению 
+    
     path('product/<slug:product_slug>/', views.product, name = 'product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import get_list_or_404, render
 from goods.models import Products
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+
+    page = request.GET.get("page", 1) # GET - словарь, а get - уже метод, применяемыый к этому словарю
 
     if category_slug == "all":
         goods = Products.objects.all() # получение всех товаров из базы данных
@@ -12,7 +14,7 @@ def catalog(request, category_slug, page=1):
         # иначе получаем товары по определенной категории
     
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "HC - Каталог",
