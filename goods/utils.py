571b67458acf08d9dab572f469bcc33d16617098
DmitriyArcher1 <d.kartrajt@gmail.com>
2024-07-30 20:16:15 +0300

    Установлено соединение с базой данный PostgreSQL & реализован полнотекстовый поиск

--- goods/utils.py
+++ goods/utils.py
@@ -1,6 +1,7 @@
 # Поиск по сайту
 import re
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchVector
 
 from goods.models import Products
 
@@ -11,12 +12,17 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
+    # q_objects = Q()
     
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
     
-    return Products.objects.filter(q_objects)
+    # return Products.objects.filter(q_objects)
