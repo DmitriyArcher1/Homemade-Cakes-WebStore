a7719c4fc871a2285fbd3d17c253327d95821fc9
DmitriyArcher1 <d.kartrajt@gmail.com>
2024-08-07 16:18:24 +0300

    Авторизация пользователя готова.

--- app/settings.py
+++ app/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import django
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent # содержит в себе путь к корневой директории
 
@@ -155,3 +156,5 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User' # говорю django, чтобы он определял именно мою таблицу auth_user
--- users/views.py
+++ users/views.py
@@ -1,13 +1,38 @@
+import re
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+from users.models import User
 
 
 def login(request):
+    
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST) # передаём словарь с данными
+
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            # проверка на то, есть ли уже такой пользователь или нет
+
+            # если возвращается объект user(он есть в БД)
+            if user:
+                auth.login(request, user) # то авторизуем его
+                return HttpResponseRedirect(reverse('main:index')) # и перенаправляем на главную страницу
+    
+    else:
+        form = UserLoginForm()
+            
     context = {
-        'title': 'HC - Авторизация'
+        'title': 'HC - Авторизация',
+        'form': form,
     }
 
     return render(request, 'users/login.html', context)
-
 
 def registration(request):
     context = {
